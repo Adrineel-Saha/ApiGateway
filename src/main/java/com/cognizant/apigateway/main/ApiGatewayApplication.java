02eb306488df55b0c0be2c388356db3bf8f95393
Adrineel-Saha <adrineelsaha@gmail.com>
2026-03-12 15:55:56 +0530

    Added API Documentation for API Gateway

--- src/main/java/com/cognizant/apigateway/main/ApiGatewayApplication.java
+++ src/main/java/com/cognizant/apigateway/main/ApiGatewayApplication.java
@@ -1,11 +1,20 @@
 package com.cognizant.apigateway.main;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient(autoRegister=true)
+@OpenAPIDefinition(
+		info=@Info(
+				title="API Gateway REST API Documentation",
+				description="REST API Documentation for API Gateway",
+				version="v1.0"
+		)
+)
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
