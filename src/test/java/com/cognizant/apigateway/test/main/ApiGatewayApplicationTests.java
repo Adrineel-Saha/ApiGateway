e27a7343b3a9e5f14d8f99789b759d8dccff9acc
Adrineel-Saha <adrineelsaha@gmail.com>
2026-03-12 18:06:14 +0530

    Modified Test cases for API Gateway.

--- src/test/java/com/cognizant/apigateway/test/main/ApiGatewayApplicationTests.java
+++ src/test/java/com/cognizant/apigateway/test/main/ApiGatewayApplicationTests.java
@@ -1,9 +1,10 @@
 package com.cognizant.apigateway.test.main;
 
+import com.cognizant.apigateway.main.ApiGatewayApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes= ApiGatewayApplication.class)
 class ApiGatewayApplicationTests {
 
 	@Test
