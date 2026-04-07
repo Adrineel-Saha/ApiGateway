3b9b6fe00149cc5570f60e86d21f1ece32cfa419
Adrineel-Saha <adrineelsaha@gmail.com>
2026-04-07 10:00:21 +0530

    Added Filter for Angular

--- src/main/java/com/cognizant/apigateway/main/ApiGatewayApplication.java
+++ src/main/java/com/cognizant/apigateway/main/ApiGatewayApplication.java
@@ -2,9 +2,15 @@ package com.cognizant.apigateway.main;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 
 @SpringBootApplication
 @EnableDiscoveryClient(autoRegister=true)
@@ -21,4 +27,28 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<Filter> corsPreflightFilter() {
+		FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>();
+
+		bean.setFilter((request, response, chain) -> {
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpServletResponse res = (HttpServletResponse) response;
+
+			if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+				res.setStatus(HttpServletResponse.SC_OK);
+				res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+				res.setHeader("Access-Control-Allow-Methods",
+						"GET,POST,PUT,DELETE,OPTIONS");
+				res.setHeader("Access-Control-Allow-Headers", "*");
+				res.setHeader("Access-Control-Allow-Credentials", "true");
+				return;
+			}
+
+			chain.doFilter(request, response);
+		});
+
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
 }
